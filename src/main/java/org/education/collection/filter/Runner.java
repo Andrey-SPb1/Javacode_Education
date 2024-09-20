7944e55b49464afcb893979a640391095e772faf
Andrey <honor8x98@gmail.com>
2024-09-20 14:07:19 +0300

    Fix filter

--- src/main/java/org/education/collection/filter/Runner.java
+++ src/main/java/org/education/collection/filter/Runner.java
@@ -1,13 +1,14 @@
 package org.education.collection.filter;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Runner {
 
     public static void main(String[] args) {
         ArrayFilter arrayFilter = new ArrayFilter();
 
-        Object[] objects = arrayFilter.filter(new Integer[]{1,2,3,4,5}, new PrintFilter());
+        Object[] objects = arrayFilter.filter(new Integer[]{1, null, 3, null, 5}, Objects::nonNull);
         System.out.println(Arrays.toString(objects));
     }
 }
